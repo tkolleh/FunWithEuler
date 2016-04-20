889f3f9250c6525de6a2d60b0060bc88f4d20946
tkolleh <tinatuh@gmail.com>
2016-04-20 12:38:32 -0400

    testing for performance and outliers

--- palindromes_base.py
+++ palindromes_base.py
@@ -2,12 +2,14 @@ import math
 
 
 def answer(n):
-    if (n < 0 or n > 1000): raise Exception("Parameter out of bounds")
+    if (n < 0 or n > 1000):
+        raise Exception("Parameter out of bounds")
+
     b_number = int(str(bin(n))[2:])
-    base = 0
+    base = 1
     palindrome = False
     while not palindrome:
-        base += 2
+        base += 1
         if base == 10:
             if is_palindrome(n, base):
                 palindrome = True
@@ -25,12 +27,16 @@ def is_palindrome(number, base):
         rev = rev * base + digit
         number = math.floor(number / base)
 
-    return forward == rev
+    return (forward == rev)
 
+num = 0
+while num < 1002:
+    print('Palindrome base for '+str(num)+' is '+str(answer(num)))
+    num += 1
 
-print('Palindrome for 0 is '+str(answer(0)))
-print('Palindrome for 3 is '+str(answer(3)))
-print('Palindrome for 42 is '+str(answer(42)))
-print('Palindrome for 97 is '+str(answer(97)))
-print('Palindrome for 999 is '+str(answer(999)))
+# print('Palindrome for 0 is '+str(answer(0)))
+# print('Palindrome for 3 is '+str(answer(3)))
+# print('Palindrome for 42 is '+str(answer(42)))
+# print('Palindrome for 97 is '+str(answer(97)))
+# print('Palindrome for 999 is '+str(answer(999)))
 #print('Palindrome for 1097 is '+str(answer(1097)))

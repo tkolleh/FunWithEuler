50e7b5f84f46d58b49f17126f1b8ea0dba156d11
tkolleh <tinatuh@gmail.com>
2016-04-21 10:26:06 -0400

    solved

--- palindromes_base.py
+++ palindromes_base.py
@@ -1,3 +1,8 @@
+""" Problem 1.
+ Solve for the smallest base of a number where its a palindrome.
+ So for 999 return 10 for base 10 and for 0 return 2 for base 2
+"""
+
 import math
 
 
@@ -5,21 +10,12 @@ def answer(n):
     if (n < 0 or n > 1000):
         raise Exception("Parameter out of bounds")
 
-    b_number = int(str(bin(n))[2:])
-
-    if len(list(str(b_number))) % 2 != 0:
-        print('\nnumber {} has an odd binary {}'.format(n, b_number))
-
     base = 1
     palindrome = False
     while not palindrome:
         base += 1
-        if base == 10:
-            if is_palindrome(n, base):
-                palindrome = True
-        elif is_palindrome(b_number, int(str(bin(base))[2:])):
-                palindrome = True
-
+        if is_palindrome(n, base):
+            palindrome = True
     return base
 
 
@@ -30,17 +26,9 @@ def is_palindrome(number, base):
         digit = math.floor(number % base)
         rev = rev * base + digit
         number = math.floor(number / base)
-
     return (forward == rev)
 
-# num = 0
-# while num < 1002:
-#     print('Palindrome base for '+str(num)+' is '+str(answer(num)))
-#     num += 1
-
-print('Palindrome for 0 is '+str(answer(0)))
-print('Palindrome for 3 is '+str(answer(3)))
-print('Palindrome for 42 is '+str(answer(42)))
-print('Palindrome for 97 is '+str(answer(97)))
-print('Palindrome for 999 is '+str(answer(999)))
-#print('Palindrome for 1097 is '+str(answer(1097)))
+num = 0
+while num < 1002:
+    print('Palindrome base for '+str(num)+' is '+str(answer(num)))
+    num += 1
